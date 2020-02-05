93ea5929dd251a7adbfe05850a2a3e4663fefc51
oyh980707 <1135219066@qq.com>
2020-02-05 20:58:53 +0800

    完成用户请求不同资源时可以根据其类型分返回正确的响应头

--- src/main/java/com/loveoyh/webserver/http/HttpResponse.java
+++ src/main/java/com/loveoyh/webserver/http/HttpResponse.java
@@ -2,6 +2,9 @@ package com.loveoyh.webserver.http;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 响应对象
@@ -14,6 +17,7 @@ public class HttpResponse {
     private int statusCode = 200;
 
     /** 响应头相关信息定义 */
+    Map<String, String> headers = new HashMap<String, String>();
 
     /** 响应正文相关信息定义 */
     private File entity;
@@ -44,7 +48,7 @@ public class HttpResponse {
      * 发送状态行
      */
     private void sendStatusLine(){
-        String line = "HTTP/1.1 "+this.statusCode+" OK";
+        String line = "HTTP/1.1 "+this.statusCode+" "+HttpContext.getStatusReason(this.statusCode);
         println(line);
         System.out.println("发送状态行:"+line);
     }
@@ -53,13 +57,12 @@ public class HttpResponse {
      * 发送响应头
      */
     private void sendHeaders(){
-        String line = "Content-Type: text/html";
-        println(line);
-        System.out.println("发送响应头:"+line);
-
-        line = "Content-Length: "+entity.length();
-        println(line);
-        System.out.println("发送响应头:"+line);
+        Set<Map.Entry<String,String>> set =  headers.entrySet();
+        for (Map.Entry<String, String> item: set){
+            String line = item.getKey()+": "+item.getValue();
+            println(line);
+            System.out.println("发送响应头:"+line);
+        }
 
         println("");
         System.out.println("响应头发送完毕!");
@@ -107,6 +110,25 @@ public class HttpResponse {
     }
 
     public void setEntity(File entity) {
+        if(entity != null){
+            /*
+             * 对应添加两个针对该形影正文的响应头
+             * Content-Type:说明该文件类型
+             * Content-Length:说明文件长度
+             */
+            headers.put("Content-Length",entity.length()+"");
+            //设置Content-Type要根据文件后缀决定值是什么
+            /*
+             * 获取文件名的后缀
+             * 思路：
+             * 从文件名最后一个"."之后的第一个字符开始截取到末尾
+             */
+            String name = entity.getName();
+            int index = name.lastIndexOf(".")+1;
+            String ext = name.substring(index);
+            String type = HttpContext.getMimeType(ext);
+            headers.put("Content-Type", type);
+        }
         this.entity = entity;
     }
 }
