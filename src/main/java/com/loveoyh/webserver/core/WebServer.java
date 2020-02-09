55282158337a1b2daec246f920e55a8acc63223c
oyh980707 <1135219066@qq.com>
2020-02-09 12:36:07 +0800

    使用线程池管理用于处理客户端请求的线程

--- src/main/java/com/loveoyh/webserver/core/WebServer.java
+++ src/main/java/com/loveoyh/webserver/core/WebServer.java
@@ -3,6 +3,8 @@ package com.loveoyh.webserver.core;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * WebServer主类
@@ -11,11 +13,14 @@ import java.net.Socket;
 public class WebServer {
     private ServerSocket server;
 
+    private ExecutorService threadPool;
+
     public WebServer() {
         try {
             System.out.println("服务端启动中...");
             //申请服务器端口
             server = new ServerSocket(8080);
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务端启动成功!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,8 +37,9 @@ public class WebServer {
                 Socket socket = server.accept();
                 System.out.println("客户端连接成功!");
 
-                Thread t = new Thread(new ClientHandler(socket));
-                t.start();
+                ClientHandler handler = new ClientHandler((socket));
+                //将ClientHandler交给线程池处理
+                threadPool.execute(handler);
 
                 System.out.println("完毕!");
             } catch (IOException e) {
